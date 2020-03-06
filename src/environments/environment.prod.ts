fdf0d0753d341c8d1c0d9dc7a656426e02854f56
Alessia Amitrano <alessia.amitrano@imagination.com>
2020-03-06 11:58:11 +0000

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   linkPreviewKey:
-    'http://api.linkpreview.net/?key=8effd7b1c1426f18a868e77098a34f13&q='
+    'https://api.linkpreview.net/?key=8effd7b1c1426f18a868e77098a34f13&q='
 };
